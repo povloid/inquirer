33f2bdaef43b9f6e2a02f37b25754eb7abf0dbfb
povloid <t34box@gmail.com>
2013-02-13 15:12:48 +0600

    Слегка подкорректировал

--- inquirer/src/main/java/pk/home/inquirer/domain/security/UserAuthoritys.java
+++ inquirer/src/main/java/pk/home/inquirer/domain/security/UserAuthoritys.java
@@ -11,29 +11,5 @@ public enum UserAuthoritys {
 	ROLE_ADMIN, // Администратор
 
 	ROLE_USER,
-
-	ROLE_DISPATCHER, // Диспетчер
-
-	ROLE_RB,
-	ROLE_RB_TRANSPORT, // Работа со справочником транспорта
-	ROLE_RB_PATH, // Работа со справочниками пути
-	ROLE_RB_ORGANIZATION, // Работа со справочниками организации
-	ROLE_RB_CUSTOMER, // Работа со справочником клиентов
-
-	ROLE_OP,
-	ROLE_OP_CASSIR_SALE, // Операции кассир продажа билета
-	ROLE_OP_CASSIR_SALE_DISCOUNT, // Операции кассир продажа билета
-	ROLE_OP_CASSIR_RETURN, // Операции кассир возврат билета
-
-	ROLE_USER_META_EDITOR, // Редактор метатегов
-
 	ROLE_VIEWER, // Просмотр
-	
-	ROLE_REPORTS,
-	ROLE_REPORTS_DIVISION,
-	ROLE_REPORTS_DIVISION_BALANCE_F1,
-	
-	ROLE_REPORTS_DRIVER,
-	ROLE_REPORTS_DRIVER_FORM1,
-	ROLE_REPORTS_DRIVER_FORM2
 }
